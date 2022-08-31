7b3200b41373d9e7043488f52989a76d133a07b6
de-oz <101826623+de-oz@users.noreply.github.com>
2022-09-01 01:38:52 +0300

    Migrated from Vue CLI to Vite

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/reset.css'
+import './reset.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
